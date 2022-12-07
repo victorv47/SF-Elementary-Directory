ea710201f9c17afe4319adb26dff77420efb278f
victorv47 <victor.vela94@gmail.com>
2022-12-06 22:53:32 -0600

    Updated department info

--- src/components/DepartmentsPage.vue
+++ src/components/DepartmentsPage.vue
@@ -5,83 +5,56 @@ import { ref } from 'vue'
 <template>
   <h1 class="flex items-center justify-center py-8 text-6xl font-thin text-slate-800">Departments</h1>
   <div class="department">
-    <div class="form">
-      <label for="firstName">First Name</label>
-    </div>
-    <div class="form">
-      <label for="lastName">Last Name</label>
-    </div>
-    <div class="form">
-      <label for="email">Email</label>
-    </div>
-    <div class="form">
-      <label for="phoneNumber">Phone Number</label>
-    </div>
-
-    <h1>Child Information</h1>
-    <div class="form">
-      <label for="firstName">First Name</label>
-    </div>
-    <div class="form">
-      <label for="lastName">Last Name</label>
-    </div>
-
-    <h1>Address Information</h1>
-    <div class="form">
-      <label for="streetName">Street Name</label>
-    </div>
-    <div class="form">
-      <label for="streetNumber">Street Number</label>
-    </div>
-    <div class="form">
-      <label for="city">City</label>
-    </div>
-    <div class="form">
-      <label for="postcode">Postcode</label>
-    </div>
+    <h1 class="h1">Academic Services</h1>
+    <h2 class="h2">(410) 875-9875</h2>
+  </div>
+  <div class="department">
+    <h1 class="h1">After School Program</h1>
+    <h2 class="h2">(410) 875-9863</h2>
+  </div>
+  <div class="department">
+    <h1 class="h1">Counseling</h1>
+    <h2 class="h2">(410) 875-8547</h2>
+  </div>
+  <div class="department">
+    <h1 class="h1">Counseling</h1>
+    <h2 class="h2">(410) 875-8547</h2>
+  </div>
+  <div class="department">
+    <h1 class="h1">Human Resources</h1>
+    <h2 class="h2">(410) 875-8572</h2>
+  </div>
+  <div class="department">
+    <h1 class="h1">Payroll</h1>
+    <h2 class="h2">(410) 875-9580</h2>
+  </div>
+  <div class="department">
+    <h1 class="h1">Risk Management</h1>
+    <h2 class="h2">(410) 875-5120</h2>
+  </div>
+  <div class="department">
+    <h1 class="h1">Technology</h1>
+    <h2 class="h2">(410) 875-5120</h2>
   </div>
 </template>
 
 <style lang="postcss">
-.app {
-  text-align: center;
-  font-size: large;
+.department {
+  border: 1px;
+  border-style: dashed;
+  border-color: black;
+  flex: auto;
+  font-size: x-large;
+  margin: auto;
+  width: 900px;
+  flex-direction: row;
+  contain: content;
 }
-.enrollment-form {
-  margin: 5px auto;
-  padding: 10px;
-  max-width: 500px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.form {
-  padding-bottom: 10px;
-  width: 100%;
+.h1 {
   text-align: left;
 }
-label {
-  display: block;
-  font-weight: bold;
-}
-
-input {
-  padding: 10px;
-  width: 100%;
-  border: 1px solid #fff;
-  border-radius: 5px;
-  outline: 0;
-  background: #f8edcf;
-}
-
-button {
-  margin-top: 30px;
-  background: rgb(20 83 45);
-  padding: 10px;
-  width: 100%;
-  color: black;
-  border: 1px solid black;
-  border-radius: 5px;
-  outline: 0;
+.h2 {
+  text-align: right;
+  margin: 0 auto;
 }
 </style>
